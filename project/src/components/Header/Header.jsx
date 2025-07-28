08337622e14e449d47cec0340afe958ce1396cc8
artem34 <art.cheremisin@gmail.com>
2025-07-28 23:51:51 +0300

    header navbar profile

---
+++ project/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyKuHk3jgH4RIU996JDVTscAPtdP2Ysm7nTg&s" />{" "}
+      <p>My Social Network</p>
+    </header>
+  );
+};
+
+export default Header;
---
+++ project/src/components/Profile/Profile.jsx
@@ -0,0 +1,17 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
