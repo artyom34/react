f096bb1276d86ab3c9ccce879e2b89be0cb80d5f
artem34 <art.cheremisin@gmail.com>
2025-08-13 14:13:06 +0300

    added component Settings

--- project/src/App.js
+++ project/src/App.js
@@ -6,19 +6,20 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
       <Navbar />
-
       <div className="app-wrapper-content">
         <Routes>
           <Route path="/profile" element={<Profile />} />
           <Route path="/dialogs" element={<Dialogs />} />
           <Route path="/news" element={<News />} />
           <Route path="/music" element={<Music />} />
+          <Route path="/settings" element={<Settings />} />
           {/* Add more routes as needed */}
         </Routes>
       </div>
---
+++ project/src/components/Settings/Settings.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./settings.module.css";
+
+const Settings = () => {
+  return (
+    <div className={classes.settings}>
+      <h2>Settings</h2>
+      {/* Add more settings options as needed */}
+    </div>
+  );
+};
+
+export default Settings;
