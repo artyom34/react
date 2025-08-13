5252dabc3cd6e4402eb67d5becbc1e81882b3769
artem34 <art.cheremisin@gmail.com>
2025-08-13 13:55:50 +0300

    set the path for the dialog and profile

--- project/src/App.js
+++ project/src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
@@ -10,8 +11,10 @@ const App = () => {
       <Header />
       <Navbar />
       <div className="app-wrapper-content">
-        <Profile />
-        <Dialogs />
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/dialogs" element={<Dialogs />} />
+        </Routes>
       </div>
     </div>
   );
